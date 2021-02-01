9c7d1d160ae028e3870ae393a777440c30ce4946
Michael <mizimizi@gmail.com>
2021-02-01 14:20:23 +0100

    first commit

---
+++ ex1/gradientDescent.m
@@ -0,0 +1,21 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+    x = X(:,2);
+    
+    hypoyhesis = theta(1) + theta(2).*x;
+    theta_zero = theta(1) - (alpha .* 1/m * (sum(hypoyhesis-y)));
+    theta_one = theta(2) - (alpha .* 1/m * (sum((hypoyhesis-y).*x)));   
+    theta = [theta_zero; theta_one];
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);   
+end
+
+end
